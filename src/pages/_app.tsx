a49f98559f5dc3caec7171f8dfbfcaaffc230b59
Juan Vieira <juan.vieira@parcelpoint.com.au>
2021-08-12 16:45:32 +1000

    adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script data-ad-client="ca-pub-5579806470669973" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
